cd50a183bbf8055121c591fb8639ee2459b6ae0f
Chirag Sharma <chiragsharma75757@gmail.com>
2023-02-28 10:53:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let timerInterval=null;
+
+function startTimer() {
+const hours=parseInt(document.getElementById('hours').value);
+const minutes=parseInt(document.getElementById('minutes').value);
+const seconds=parseInt(document.getElementById('seconds').value);
+console.log(hours, minutes, seconds);
+
+let totalSeconds = hours * 3600 + minutes * 60 + seconds;
+console.log(totalSeconds);
+
+if(totalSeconds <= 0){
+    alert('please enter a valid time');
+    return;
+}
+
+timerInterval = setInterval(()=>{
+    // 3600 -> 1hr 60sec
+    const hoursLeft=Math.floor(totalSeconds/3600);
+    const minutesLeft=Math.floor((totalSeconds % 3600) / 60);
+    const secondsLeft=totalSeconds % 60;
+    
+    document.getElementById('timer').textContent = `${hoursLeft.toString().padStart(2, '0')}:${minutesLeft.toString().padStart(2, '0')}:${secondsLeft.toString().padStart(2,'0')}`;
+
+    if(totalSeconds<=0){
+        clearInterval(timerInterval);
+        document.getElementById('timer').textContent = 'Timer\'s Up!';
+        document.getElementById('audio').play();
+    } else {
+        totalSeconds--;
+    }
+}, 1000);
+}
+
+function stopTimer () {
+clearInterval(timerInterval);
+document.getElementById('timer').textContent = '';
+}
+
+document.getElementById('start-button').addEventListener('click',startTimer);
+document.getElementById('stop-button').addEventListener('click',stopTimer);
